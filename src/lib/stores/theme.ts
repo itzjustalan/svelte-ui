501f45bdca2fc1cabfcce0956271d1b588ba75c5
halid <h@gmail.com>
2023-03-07 18:00:29 +0530

    theme bug is fixed

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -2,43 +2,44 @@ import { writable } from "svelte/store";
 import { browser } from "$app/environment";
 import { getCookieValue } from "$lib/utils";
 
-const themeCookieAttributes = 'SameSite=Strict;max-age=31536000;path="/"';
+const themeCookieAttributes = 'SameSite=Strict;max-age=31536000;path=/';
 const themeCookieName = "app-theme";
 export enum Theme {
-    Dark = "dark",
-    Light = "light",
+  Dark = "dark",
+  Light = "light",
 }
 
 function createStore() {
-    let initialTheme = Theme.Light;
-    if (browser) {
-        const storedTheme = getCookieValue(themeCookieName, document.cookie);
-        if (storedTheme) {
-            initialTheme = storedTheme as Theme;
-        } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            initialTheme = Theme.Dark;
-        }
-        updateDocumentTheme(initialTheme);
+  let initialTheme = Theme.Light;
+  if (browser) {
+    const storedTheme = getCookieValue(themeCookieName, document.cookie);
+    if (storedTheme) {
+      initialTheme = storedTheme as Theme;
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      initialTheme = Theme.Dark;
     }
-    const { subscribe, set, update } = writable<Theme>(initialTheme);
+    updateDocumentTheme(initialTheme);
+  }
+  const { subscribe, set, update } = writable<Theme>(initialTheme);
 
-    return {
-        subscribe,
-        set: (theme: Theme) => {
-            updateDocumentTheme(theme);
-            set(theme);
-        },
-        toggle: () =>
-            update((v) =>
-                updateDocumentTheme(v === Theme.Dark ? Theme.Light : Theme.Dark)
-            ),
-    };
+  return {
+    subscribe,
+    set: (theme: Theme) => {
+      updateDocumentTheme(theme);
+      set(theme);
+    },
+    toggle: () =>
+      update((v) =>
+        updateDocumentTheme(v === Theme.Dark ? Theme.Light : Theme.Dark)
+      ),
+  };
 }
 
 function updateDocumentTheme(theme: Theme) {
-    document.documentElement.dataset.theme = theme;
-    document.cookie = `${themeCookieName}=${theme};${themeCookieAttributes}`;
-    return theme;
+  if (!browser) return theme;
+  document.documentElement.dataset.theme = theme;
+  document.cookie = `${themeCookieName}=${theme};${themeCookieAttributes}`;
+  return theme;
 }
 
 // const storedTheme = localStorage.getItem(themeCookieName);
