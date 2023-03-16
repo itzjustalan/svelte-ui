7ed6df60a49960d59a1d53f61c36464f45675b9d
itzjustalan <itzjustalan@gmail.com>
2023-03-16 20:24:28 +1300

    automagic refresh

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,7 +1,8 @@
 import { log } from "$lib/logger";
 import type { Handle, HandleServerError } from "@sveltejs/kit";
 
-export const handleError: HandleServerError = async ({ event, error }) => {
+export const handleError: HandleServerError = async (input) => {
     log.error('client??', 22, { H: "fsdf"}, 'hooks.client.ts')
+    // log.warn(input)
     return Error('client error');
 }
--- src/lib/networks/auth.network.ts
+++ src/lib/networks/auth.network.ts
@@ -1,6 +1,7 @@
 import { type AuthData, authSchema } from "$lib/zod/schemas/user.signup";
 import type { User } from "$lib/zod/models/user.model";
 import defaultApi from "./apis";
+import { decodeJwt } from "$lib/utils";
 
 interface AuthResponse {
   user: User;
@@ -11,14 +12,35 @@ interface AuthResponse {
 }
 
 class AuthNetwork {
-  signin = async (data: AuthData): Promise<AuthResponse> =>
-    (await defaultApi.post("v1/api/auth/signin", authSchema.parse(data))).data;
+  accessTimeout: NodeJS.Timeout | undefined;
 
-//   async signin(data: AuthData): Promise<AuthResponse> {
-//     authSchema.parse(data);
-//     return (await defaultApi.post<AuthResponse>("v1/api/auth/signin", data))
-//       .data;
-//   }
+  // refresh = async () => await defaultApi.get('v1/api/auth/refresh');
+  // signin = async (data: AuthData): Promise<AuthResponse> =>
+  //   (await defaultApi.post("v1/api/auth/signin", authSchema.parse(data))).data;
+
+  signout = () => clearInterval(this.accessTimeout);
+
+  signin = async (data: AuthData): Promise<AuthResponse> => {
+    authSchema.parse(data);
+    const res = await defaultApi.post<AuthResponse>("v1/api/auth/signin", data);
+    this.autoRefresh(res.data.jwt.accessToken);
+    return res.data;
+  };
+
+  refresh = async (): Promise<AuthResponse> => {
+    const response = await defaultApi.get<AuthResponse>("v1/api/auth/refresh");
+    this.autoRefresh(response.data.jwt.accessToken);
+    return response.data;
+  };
+
+  autoRefresh = (accessToken: string) => {
+    const decodedToken = decodeJwt(accessToken);
+    clearTimeout(this.accessTimeout);
+    this.accessTimeout = setTimeout(
+      this.refresh,
+      (decodedToken.exp - decodedToken.iat) * 1000,
+    );
+  }
 }
 
 export const authNetwork = new AuthNetwork();
