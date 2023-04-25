3c103e73f6d8caafa835bd04f135cb4911a34e02
halid <h@gmail.com>
2023-04-25 10:07:12 +0530

    skelton ui

--- src/routes/v1/+layout.svelte
+++ src/routes/v1/+layout.svelte
@@ -1,4 +1,13 @@
 <script lang="ts">
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/all.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../../app.postcss';
+
 	import Header from './Header.svelte';
 	import './styles.css';
 
